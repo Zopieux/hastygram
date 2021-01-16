6791786f194252ac42d4fb800338440cf6099821
Alexandre Macabies <web+oss@zopieux.com>
2021-01-17 00:51:47 +0100

    Add README and GitHub action for packaging.

--- hastygram/app.py
+++ hastygram/app.py
@@ -15,16 +15,6 @@ from hastygram import api
 router = APIRouter()
 
 
-# from starlette.middleware.cors import CORSMiddleware
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3000"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-
 @dataclasses.dataclass(frozen=True)
 class URLProfile:
     username: str
